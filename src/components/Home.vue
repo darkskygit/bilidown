4e161ad53161e96b653d4450d03d6d0f23c20096
DarkSky <darksky2048@gmail.com>
2018-04-25 19:16:59 +0800

    entry json

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,10 +23,20 @@ import * as platform from "tns-core-modules/platform";
 import * as color from "tns-core-modules/color";
 import { android } from "tns-core-modules/application";
 import { getJSON, getString } from "tns-core-modules/http";
+import { alert } from "tns-core-modules/ui/dialogs";
 import md5 from "js-md5";
 
 class BilibiliApi {
-  static appSecret() {
+  static GetClientType() {
+    return ["tv.danmaku.bili", "com.bilibili.app.blue", "com.bilibili.app.in"];
+  }
+  static GetClientPackage(type) {
+    return this.GetClientType()[type];
+  }
+  static GetAppPath(type) {
+    return `/sdcard/Android/data/${this.GetClientPackage(type)}/`;
+  }
+  static AppSecret() {
     return "560c52ccd288fed045859ed18bffd973";
   }
   static GenSign(url, secret) {
@@ -38,8 +48,9 @@ class BilibiliApi {
     return md5(sort.join("&") + secret);
   }
   static VideoInfo(source_id) {
-    let url = `http://app.bilibili.com/x/view?aid=${source_id}&appkey=1d8b6e7d45233436&build=521000&ts=${new Date().getTime()}`;
-    return getJSON(`${url}&sign=${this.GenSign(url, this.appSecret())}`);
+    let url = `http://app.bilibili.com/x/view?aid=${source_id}&appkey=1d8b6e7d45233436&build=521000&ts=${new Date().getTime() /
+      1000}`;
+    return getJSON(`${url}&sign=${this.GenSign(url, this.AppSecret())}`);
   }
   static BengumiInfo(source_id) {
     let url = `https://bangumi.bilibili.com/view/web_api/season?season_id=${source_id}`;
@@ -57,18 +68,113 @@ class BilibiliApi {
         }
       });
   }
+  static GetQuality(qtype) {
+    const quality = [
+      {
+        quality: "lua.mp4.bb2api.16",
+        description: "清晰",
+        video_quality: 16,
+        q: 1
+      },
+      {
+        quality: "lua.flv720.bb2api.64",
+        description: "高清",
+        video_quality: 64,
+        q: 2
+      },
+      {
+        quality: "lua.flv.bb2api.80",
+        description: "超清",
+        video_quality: 80,
+        q: 3
+      },
+      {
+        quality: "lua.hdflv2.bb2api.bd",
+        description: "1080P",
+        q: 4,
+        video_quality: 112
+      }
+    ];
+    return quality[qtype];
+  }
+  static GenDownItem(qtype, item, data) {
+    let time = new Date().getTime();
+    let quality = this.GetQuality(qtype);
+    let entry = data.isEp
+      ? {
+          season_id: data.season_id,
+          title: data.title,
+          cover: data.cover,
+          type_tag: quality.quality,
+          time_create_stamp: time,
+          time_update_stamp: time,
+          prefered_video_quality: quality.video_quality,
+          source: {
+            av_id: item.aid,
+            cid: item.cid,
+            website: "bangumi",
+            webvideo_id: ""
+          },
+          ep: {
+            av_id: item.aid,
+            danmaku: item.cid,
+            cover: item.cover,
+            episode_id: item.ep_id,
+            index: item.index,
+            index_title: item.index_title,
+            page: item.page,
+            has_alias: false
+          }
+        }
+      : {
+          avid: this.source_id,
+          title: data.title,
+          cover: data.pic,
+          type_tag: quality.quality,
+          time_create_stamp: time,
+          time_update_stamp: time,
+          page_data: {
+            cid: item.cid,
+            from: item.from,
+            part: item.part,
+            vid: item.vid,
+            tid: data.tid,
+            page: item.page
+          }
+        };
+    let finalentry = {
+      it_completed: false,
+      total_bytes: 0,
+      downloaded_bytes: 0,
+      guessed_total_bytes: 0,
+      total_time_milli: 0,
+      danmaku_count: 3000
+    };
+    let downitem = {
+      downPath: BilibiliApi.GetAppPath(0),
+      av_id: data.source_id,
+      episode_id: data.isEp ? item.ep_id : item.page,
+      entry_content: Object.assign(finalentry, entry),
+      quality: quality.quality,
+      index_content: ""
+    };
+    return downitem;
+  }
 }
 
 export default {
   data() {
     return {
-      dllist: null
+      dllist: null,
+      source_id: null
     };
   },
   computed: {
     itemlist() {
       if (this.dllist && typeof this.dllist === "object") {
         if (this.dllist.episodes) this.dllist.isEp = true;
+        if (this.source_id && typeof this.source_id === "object")
+          this.dllist.source_id = this.source_id[0];
         return this.dllist.pages || this.dllist.episodes;
       }
       return false;
@@ -96,30 +202,36 @@ export default {
       //   let info = this.GetLinkInfo("ep183799");
       //   console.log("GetLinkInfo", info)
       // });
-      this.GetLinkInfo("ep183799").then(data => console.log(this.dllist = data));
+      this.GetLinkInfo("ep183799").then(data => {
+        if (data) {
+          if (data.title.indexOf("僅") !== -1)
+            alert("你下载的是地区专供番，暂未支持下载");
+          this.dllist = data;
+        } else alert("获取数据失败，请检查链接或视频号是否正确");
+      });
     },
     GetLinkInfo(content) {
       if (typeof content !== "string") return;
       let source_id = content.match(/\d{1,9}/);
       if (!isNaN(source_id)) {
-        let promise = this.Processer(content)(source_id);
+        let promise = this.Processer(content, source_id);
         return promise.then(({ data, result }) => data || result);
       }
     },
-    Processer(content) {
-      return this.GetLinkProcesser(content).bind(BilibiliApi);
+    Processer(content, source_id) {
+      this.source_id = source_id;
+      return this.GetLinkProcesser(content).call(BilibiliApi, source_id);
     },
     GetLinkProcesser(content) {
       if (content.indexOf("av") !== -1) return BilibiliApi.VideoInfo;
-      else if (content.indexOf("ep") !== -1)
-        return BilibiliApi.EPBengumiInfo;
+      else if (content.indexOf("ep") !== -1) return BilibiliApi.EPBengumiInfo;
       else return BilibiliApi.BengumiInfo;
     },
     itemtitle(item) {
       return (item.index || item.page) + "|" + (item.part || item.index_title);
     },
     onButtonTap(item) {
-      console.log(item);
+      console.log(BilibiliApi.GenDownItem(0, item, this.dllist));
     }
   }
 };
