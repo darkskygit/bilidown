e8b34e74e51f716b79c596546f371f10a921e728
DarkSky <darksky2048@gmail.com>
2018-04-27 12:11:27 +0800

    basic function completed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,12 @@
   <Page class="page" backgroundSpanUnderStatusBar="true" color="#2c303a">
     <ActionBar class="action-bar btn-primary" title="哔哩下载工具"/>
     <StackLayout>
-      <Button class="btn btn-primary" @tap="clipbroad">clipbroad</Button>
+      <StackLayout orientation="horizontal">
+        <Button class="btn btn-primary" @tap="clipbroad">从粘贴板读取</Button>
+        <Button class="btn btn-primary" @tap="selectQuality">{{ GetQuality }}</Button>
+        <Button class="btn btn-primary" @tap="selectClient">{{ GetClient }}</Button>
+      </StackLayout>
+      <ActivityIndicator :busy="loading" />
       <StackLayout v-if="dllist">
         <FlexboxLayout justifyContent="center">
           <Label :text="dllist.title" />
@@ -24,56 +29,56 @@ import * as platform from "tns-core-modules/platform";
 import * as color from "tns-core-modules/color";
 import { android as os } from "tns-core-modules/application";
 import { getJSON, getString } from "tns-core-modules/http";
-import { alert } from "tns-core-modules/ui/dialogs";
+import { alert, action } from "tns-core-modules/ui/dialogs";
 import { Folder } from "tns-core-modules/file-system";
 import md5 from "js-md5";
+
+const ClientName = [
+  { type: "tv.danmaku.bili", name: "正式版" },
+  { type: "com.bilibili.app.blue", name: "概念版" },
+  { type: "com.bilibili.app.in", name: "Play版" }
+];
+
+const Quality = [
+  {
+    quality: "lua.mp4.bb2api.16",
+    description: "清晰",
+    video_quality: 16,
+    q: 1
+  },
+  {
+    quality: "lua.flv720.bb2api.64",
+    description: "高清",
+    video_quality: 64,
+    q: 2
+  },
+  {
+    quality: "lua.flv.bb2api.80",
+    description: "超清",
+    video_quality: 80,
+    q: 3
+  },
+  {
+    quality: "lua.hdflv2.bb2api.bd",
+    description: "1080P",
+    q: 4,
+    video_quality: 112
+  }
+];
 
 class BilibiliApi {
   constructor(content) {
     this.content = content;
     return { api: this, info: this.GetLinkInfo() };
   }
-  GetClientType() {
-    return ["tv.danmaku.bili", "com.bilibili.app.blue", "com.bilibili.app.in"];
-  }
-
-  GetClientPackage(type) {
-    return this.GetClientType()[type];
-  }
 
   GetAppPath(type) {
     let path = android.os.Environment.getExternalStorageDirectory();
-    return `${path}/Android/Data/${this.GetClientPackage(type)}/download/`;
+    return `${path}/Android/Data/${ClientName[type].type}/download/`;
   }
 
   GetQuality(qtype) {
-    const quality = [
-      {
-        quality: "lua.mp4.bb2api.16",
-        description: "清晰",
-        video_quality: 16,
-        q: 1
-      },
-      {
-        quality: "lua.flv720.bb2api.64",
-        description: "高清",
-        video_quality: 64,
-        q: 2
-      },
-      {
-        quality: "lua.flv.bb2api.80",
-        description: "超清",
-        video_quality: 80,
-        q: 3
-      },
-      {
-        quality: "lua.hdflv2.bb2api.bd",
-        description: "1080P",
-        q: 4,
-        video_quality: 112
-      }
-    ];
-    return quality[qtype];
+    return Quality[qtype];
   }
 
   GetSourceId() {
@@ -92,18 +97,21 @@ class BilibiliApi {
     let sort = params.split("&").sort();
     return md5(sort.join("&") + secret);
   }
+
   VideoInfo() {
     let url = `http://app.bilibili.com/x/view?aid=${
       this.source_id
     }&appkey=1d8b6e7d45233436&build=521000&ts=${new Date().getTime() / 1000}`;
     return getJSON(`${url}&sign=${this.GenSign(url, this.AppSecret())}`);
   }
+
   BengumiInfo() {
     let url = `https://bangumi.bilibili.com/view/web_api/season?season_id=${
       this.source_id
     }`;
     return getJSON(url);
   }
+
   EPBengumiInfo() {
     return getString(
       `https://www.bilibili.com/bangumi/play/ep${this.source_id}`
@@ -137,7 +145,7 @@ class BilibiliApi {
     else return this.BengumiInfo();
   }
 
-  GenDownItem(qtype, item, data) {
+  GenDownItem(qtype, ctype, item, data) {
     let time = new Date().getTime();
     let quality = this.GetQuality(qtype);
     let entry = data.isEp
@@ -167,7 +175,7 @@ class BilibiliApi {
           }
         }
       : {
-          avid: this.GetSourceId(),
+          avid: this.GetSourceId()[0],
           title: data.title,
           cover: data.pic,
           type_tag: quality.quality,
@@ -191,7 +199,7 @@ class BilibiliApi {
       danmaku_count: 3000
     };
     let downitem = {
-      downPath: this.GetAppPath(0),
+      downPath: this.GetAppPath(ctype),
       av_id: this.GetSourceId(),
       episode_id: data.isEp ? item.ep_id : item.page,
       entry_content: Object.assign(finalentry, entry),
@@ -205,6 +213,9 @@ class BilibiliApi {
 export default {
   data() {
     return {
+      client: 0,
+      quality: 1,
+      loading: false,
       dllist: null
     };
   },
@@ -215,6 +226,14 @@ export default {
         return this.dllist.pages || this.dllist.episodes;
       }
       return false;
+    },
+    GetQuality() {
+      let quality = Quality[this.quality] || { description: "未选择" };
+      return quality.description;
+    },
+    GetClient() {
+      let client = ClientName[this.client] || { name: "未选择" };
+      return client.name || "未选择";
     }
   },
   mounted() {
@@ -234,18 +253,39 @@ export default {
         console.log(err);
       }
     },
+    selectQuality() {
+      action("请选择清晰度", "取消", Quality.map(obj => obj.description)).then(
+        result => {
+          let item = Quality.filter(obj => obj.description === result);
+          if (item.length > 0) this.quality = Quality.indexOf(item[0]);
+        }
+      );
+    },
+    selectClient() {
+      action("请选择客户端", "取消", ClientName.map(obj => obj.name)).then(
+        result => {
+          let item = ClientName.filter(obj => obj.name === result);
+          if (item.length > 0) this.client = ClientName.indexOf(item[0]);
+        }
+      );
+    },
     clipbroad() {
-      getText().then(content => {
-        ({ api: this.api, info: this.info } = new BilibiliApi(content));
-        if (this.info && this.info instanceof Promise)
-          this.info.then(data => {
-            if (data) {
-              if (data.title.indexOf("僅") !== -1)
-                alert("你下载的是地区专供番，可能需要对应地区网络才能下载");
-              this.dllist = data;
-            } else alert("获取数据失败，请检查链接或视频号是否正确");
-          });
-      });
+      if (this.loading) return;
+      this.loading = true;
+      getText()
+        .then(content => {
+          ({ api: this.api, info: this.info } = new BilibiliApi(content));
+          if (this.info && this.info instanceof Promise)
+            this.info.then(data => {
+              if (data) {
+                if (data.title.indexOf("僅") !== -1)
+                  alert("你下载的是地区专供番，可能需要对应地区网络才能下载");
+                this.dllist = data;
+              } else alert("获取数据失败，请检查链接或视频号是否正确");
+              this.loading = false;
+            });
+        })
+        .catch(e => (this.loading = !alert(`获取数据失败：${e}`)));
     },
     onButtonTap(item) {
       requestPermission("android.permission.WRITE_EXTERNAL_STORAGE")
@@ -254,14 +294,19 @@ export default {
     },
     createDownQueue(item) {
       try {
-        let downitem = this.api.GenDownItem(0, item, this.dllist);
-        let subpath = this.dllist.isEp ? downitem.episode_id : downitem.page;
-        let path = `${downitem.downPath}s_${downitem.av_id}/${subpath}/`;
+        let dlitem = this.api.GenDownItem(
+          this.quality,
+          this.client,
+          item,
+          this.dllist
+        );
+        let mainpath = (this.dllist.isEp ? "s_" : "") + dlitem.av_id;
+        let path = `${dlitem.downPath}${mainpath}/${dlitem.episode_id}/`;
         let folder = Folder.fromPath(path);
-        let entryjson = JSON.stringify(downitem.entry_content);
+        let entryjson = JSON.stringify(dlitem.entry_content);
         folder.getFile("entry.json").writeText(entryjson);
         folder.getFile("danmaku.xml");
-        folder.getFolder(downitem.quality).getFile("index.json");
+        folder.getFolder(dlitem.quality).getFile("index.json");
         alert("创建下载下载队列成功");
       } catch (e) {
         console.log(e);
@@ -269,7 +314,11 @@ export default {
       }
     },
     itemtitle(item) {
-      return (item.index || item.page) + "|" + (item.part || item.index_title);
+      return (
+        (item.index || item.page) +
+        "|" +
+        (item.part || item.index_title || "无标题")
+      );
     }
   }
 };
